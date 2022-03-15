38973a51a3530687b844b1c6d8a54c6c03e970ba
s <shalivitalya@yandex.ru>
2022-03-15 10:23:06 +0300

    favouritesPage adapted and reworked

--- src/components/FavouritesPage/FavouritesItem/FavouritesItem.jsx
+++ src/components/FavouritesPage/FavouritesItem/FavouritesItem.jsx
@@ -1,15 +1,19 @@
 import s from "./FavouritesItem.module.css";
+import {useState} from "react";
 
 export const FavouritesItem = (props) => {
+    const [visible, setVisible] = useState(false);
     const {queryTitle, onDelete, onExecute, onEdit} = props;
+    const actionClass = visible?(s.favouritesItemActions + " "  + s.visible):s.favouritesItemActions;
     return (
-        <div className={s.favouritesItem}>
-            <span>{queryTitle}</span>
-            <div className={s.favouritesItemActions}>
-                <span onClick={onExecute}>Выполнить</span>
-                <span onClick={onEdit}>Редактировать</span>
-                <span onClick={onDelete}>Удалить</span>
-            </div>
+        <div>
+            <h2 className={s.favouritesItem} onClick={() => setVisible(!visible)}>{queryTitle}</h2>
+                <div className={actionClass}>
+                    <span onClick={onExecute}>Выполнить</span>
+                    <span onClick={onEdit}>Редактировать</span>
+                    <span onClick={onDelete}>Удалить</span>
+                </div>
         </div>
     )
 }
+
